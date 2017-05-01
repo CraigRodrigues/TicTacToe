ad79062288b8527f52344bc73c61ce31a77484b2
Craig Rodrigues <PagingCraig@gmail.com>
2017-05-01 15:14:39 -0700

    Stub out basic game functionality

--- Game.js
+++ Game.js
@@ -6,10 +6,45 @@
 // Keeps track of player names
 // Randomly picks who goes first
 
+const readline = require('readline')
+
 class Game {
   constructor (n) {
-    this.size = n
-    this.board = null
+    this.game = []
+    this.winner = ''
+    this.playerOne = ''
+    this.playerTwo = ''
+    this.active = true
+  }
+
+  getPlayerNames () {
+    // Names of players
+  }
+
+  winner () {
+    // Checks board for winner
+    // Returns name of winner and ends game
+  }
+
+
+  start () {
+    // Starts game
+  }
+
+  pickFirstPlayer () {
+    // Randomly chooses who plays first
+  }
+
+  placeMark (i, j) {
+    // Puts a mark at the coordinates specified by the player
+  }
+
+  announceWinner () {
+    // Prints game winner
+  }
+
+  gameOver () {
+    // Asks if players wish to play again
   }
 }
 
