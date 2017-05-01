08539831f468c4d2094a1539a3700148015f0df0
Craig Rodrigues <PagingCraig@gmail.com>
2017-05-01 14:42:20 -0700

    Init

---
+++ index.js
@@ -0,0 +1,14 @@
+// Tic Tac Toe
+
+// Expected Features
+// ===============
+// * Minimal UI that redraws the board and makes clear whose turn it is, each turn.
+// * Players can submit moves (assume, admittedly unrealistically, that both players are sitting at the same keyboard).
+// * Win detection - detect and display who won
+
+// Bonus / Stretch goals (any or all of the following)
+// =======================================
+// * Structure your code such that the UI can be turned easily into a native mobile app (iOS say) without having to rewrite the core game logic.
+// * Implement win detection with a functional rather than iterative style.
+// * Between moves, rotate the board 90 degrees counter-clockwise. The moves "fall" due to "gravity", post-rotation.
+
